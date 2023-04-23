b063120631b6dc10c4716ddb058c7b0e8c7342c9
Nidhin Joseph <nidhinjoseph.1990@gmail.com>
2023-04-23 01:55:03 -0300

    updates

--- run_dask_tpch.py
+++ run_dask_tpch.py
@@ -709,7 +709,7 @@ group by
 
 sql_test = """select 
     l_orderkey, 
-    cumulative_extended_prices.cumulative_price + SUM(l_extendedprice)
+    cumulative_extended_prices_cumulative_price + SUM(l_extendedprice)
   from 
     lineitem, cumulative_extended_prices
       where
