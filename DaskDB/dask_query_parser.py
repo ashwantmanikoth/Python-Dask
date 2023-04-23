d5e831aff02b3f868456762b9e0424ce1340da6e
Nidhin Joseph <nidhinjoseph.1990@gmail.com>
2023-04-23 19:33:26 -0300

    updates

--- DaskDB/dask_query_parser.py
+++ DaskDB/dask_query_parser.py
@@ -28,7 +28,7 @@ class DaskQueryParser:
             cte_pattern = r"(WITH RECURSIVE\s+(.*?)\s*\((.*?)\)\s*AS\s*\()"
             base_case_pattern = r"(\bSELECT\b.*?)(?=\b(?:UNION ALL|UNION|INTERSECT|EXCEPT)\b)"
             recursive_case_pattern = r"(\b(?:UNION ALL|UNION|INTERSECT|EXCEPT)\b(.*?))(?=\))"
-            final_query_pattern = r"(WITH RECURSIVE.*?AS\s*\([\s\S]*?\))?\s*SELECT\s+(.*?)(?=;)"
+            final_query_pattern = r"(WITH RECURSIVE.*?AS\s*\([\s\S]*?\))?\s*(SELECT\s+.*?)(?=;)"
 
             cte_groups = re.search(cte_pattern, query, re.DOTALL | re.IGNORECASE)
             self.cte = cte_groups.group(2)
