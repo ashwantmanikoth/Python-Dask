94375cd44492dfa505f5b505e5e2e460d84c285b
Nidhin Joseph <nidhinjoseph.1990@gmail.com>
2023-04-23 21:42:05 -0300

    updates

--- DaskDB/dask_plan.py
+++ DaskDB/dask_plan.py
@@ -200,6 +200,10 @@ col_names_region = ['r_regionkey','r_name','r_comment']\n"""
                
         
     def create_filter_strings(self,data_table,task,offset):
+        if 'left' not in task or 'right' not in task:
+            if task['type' == 'CONSTANT']:
+                return ['value']
+
         if task['left']['type'] == 'VARIABLE':
             left_arg = (data_table + '[' + 'self.column_mappings["' + data_table + '"][' 
             + str(task['left']['columnIdx']-offset) + ']]')
