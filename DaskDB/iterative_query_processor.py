60e630c2047699d477e7aae6c63fb10d7e45586d
Nidhin Joseph <nidhinjoseph.1990@gmail.com>
2023-04-24 04:08:37 -0300

    updates

--- DaskDB/iterative_query_processor.py
+++ DaskDB/iterative_query_processor.py
@@ -8,9 +8,6 @@ class IterativeQueryProcessor:
 
     def __init__(self, base_code_block, iterative_code_block, final_code_block, **dataframes):
         self.dataframes = dataframes
-        locals().update(self.dataframes)
-
-        # Find the CTE name from the return statement in the final method
         cte_name_match = re.search(r'(\w+)\s*=\s*\w+\s*\.\w+\s*\(\s*\)$', final_code_block, flags=re.MULTILINE)
         if cte_name_match:
             cte_name = cte_name_match.group(1)
@@ -18,20 +15,23 @@ class IterativeQueryProcessor:
             cte_name = None
 
         self.create_function("base_query", base_code_block, [])
-        self.create_function("recursive_query", iterative_code_block, [cte_name])
-        self.create_function("final_query", final_code_block, [cte_name])
+        self.create_function("recursive_query", iterative_code_block.replace("data_ml", cte_name), [cte_name])
+        self.create_function("final_query", final_code_block.replace("data_ml", cte_name), [cte_name])
 
     def create_function(self, func_name, code_block, param_names):
         func_header = f'def {func_name}(self, ' + ', '.join(param_names) + '):\n'
 
-        indented_code = '    '.join(code_block.splitlines(True))
+        statements = code_block.splitlines(True)
+        statements.insert(0, 'locals().update(self.dataframes)')
 
         last_assignment_match = re.search(r'(\w+)\s*=\s*\w+\s*\.\w+\s*\(\s*\)$', code_block, flags=re.MULTILINE)
 
         if last_assignment_match:
             return_var = last_assignment_match.group(1)
+            statements.append('return ' + return_var)
 
-        func_definition = func_header + indented_code
+        indented_code = '    '.join(statements)
+        func_definition = func_header + '    ' + indented_code
 
         exec(func_definition, globals(), locals())
         setattr(self, func_name, locals()[func_name])
@@ -45,7 +45,6 @@ class IterativeQueryProcessor:
         with self:
             cte_customer_tree = base_query(self)
             iteration = 0
-
             while True:
                 new_cte_customer_tree = recursive_query(self, cte_customer_tree)
                 if new_cte_customer_tree.empty or iteration >= max_iterations:
@@ -53,11 +52,16 @@ class IterativeQueryProcessor:
 
                 cte_customer_tree = dd.concat([cte_customer_tree, new_cte_customer_tree])
                 iteration += 1
-
-            result = final_query(self, cte_customer_tree)
-
-        return result
-
+            return final_query(self, cte_customer_tree)
 
     def add_columns_index(self, df, df_string):
         self.column_mappings[df_string] = df.columns
+
+    def __enter__(self):
+        for df_name, df in self.dataframes.items():
+            locals()[df_name] = df
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        return self
+
