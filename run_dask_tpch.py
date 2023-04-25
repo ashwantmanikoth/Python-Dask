9ca2829fb8fcdc024808fc6f10153086220ae539
Nidhin Joseph <nidhinjoseph.1990@gmail.com>
2023-04-25 08:49:56 -0300

    updates

--- run_dask_tpch.py
+++ run_dask_tpch.py
@@ -694,7 +694,8 @@ SELECT   cte_src AS through,
          cte_lvl
 FROM     cte_paths
 WHERE    cte_target = 5
-ORDER BY cte_distance LIMIT 1;
+GROUP BY cte_target
+HAVING cte_distance = MIN(cte_distance)
 """
 
 sql_test = """WITH recursive cte_customer_tree (cte_custkey, cte_customer_name, cte_revenue, cte_lvl) AS
